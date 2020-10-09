d988d802b09d4d6e608bb95e776bbc88530c41d5
Rei Armenia <anthony.armenia@its.ny.gov>
2020-10-09 14:20:30 -0400

    DOCUMENT - bucket.ts tsdoc
    added tsdoc documentation to bucket.ts

--- app/src/api/bucket/bucket.ts
+++ app/src/api/bucket/bucket.ts
@@ -1,19 +1,38 @@
+/**
+ * Used for creating a storage bucket that the bot will keep track of.
+ * @attr shouldPersist A boolean that tells the bot if it should save this bucket between sessions.
+ *
+ * @attr bucket A key-value pair object to store data in. Persistent Buckets must be compatible with JSON.stringify
+ *
+ * @optional onAddHandler A function that will handle adding data to the bucket. Default is basic assign.
+ *
+ * @public
+ */
 export class Bucket {
 
   private shouldPersist: boolean;
-  private bucket: {[key: string]: any};
+  private bucket: { [key: string]: any };
   private onAddHandler?: (key: string, data: any) => void;
 
-  constructor(data: {[key: string]: any}, shouldPersist: boolean, onAddHandler?: (key: string, data: any) => void) {
+  constructor(data: { [key: string]: any }, shouldPersist: boolean, onAddHandler?: (key: string, data: any) => void) {
     this.shouldPersist = shouldPersist;
     this.bucket = data;
     this.onAddHandler = onAddHandler;
   }
 
+  /**
+   * Get the persist object of this bucket.
+   *
+   * @returns the bucket or undefined if the bucket should not persist
+   */
   public toPersist(): any | undefined {
     return this.shouldPersist ? this.bucket : undefined;
   }
 
+  /**
+   * Loads data into the bucket if the bucket persists
+   * @param persistData the data to load
+   */
   public loadFromPersist(persistData: any): void {
     if (this.shouldPersist) {
       Object.keys(persistData).forEach((key: string) => {
@@ -22,6 +41,11 @@ export class Bucket {
     }
   }
 
+  /**
+   * Adds a key-value pair to the bucket, uses the onAddHandler if provided
+   * @param key the key to add the data at
+   * @param data the data to add
+   */
   public set(key: string, data: any): void {
 
     if (this.onAddHandler) {
@@ -32,13 +56,42 @@ export class Bucket {
 
   }
 
-  public get(key: string): any | undefined{
+  /**
+   * Gets a value for a passed key
+   * @param key the key we are trying to get the value of
+   *
+   * @returns the value at the key or undefined if no value is found
+   */
+  public get(key: string): any | undefined {
     return this.bucket[key] ?? undefined;
   }
 
+  /**
+   * Checks if the bucket contains the passed key
+   * @param key the key we are checking
+   */
   public has(key: string): boolean {
     return this.get(key) !== undefined;
   }
 
+  /**
+   * Removes the key-value pair with the passed key
+   * @param key the key to remove
+   */
+  public remove(key: string): void {
+    if (this.has(key)) {
+      delete this.bucket[key];
+    }
+  }
+
+
+  /**
+   * Gets all the keys in the bucket.
+   *
+   * @returns an array of all keys(string) in the bucket
+   */
+  public keys(): string[] {
+    return Object.keys(this.bucket);
+  }
 
 }
